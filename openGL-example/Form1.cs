f7f08e62e45c3fb856c7fa5da05a4452f43208a4
ammediagroup <andrey@ammediagroup.org>
2017-10-23 22:11:52 +0300

    first lab

---
+++ openGL-example/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace openGL_example
+{
+    public partial class Form1 : Form
+    {
+
+        private float axis_rotate_X;
+        private float axis_rotate_Y;
+        private float axis_rotate_Z;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.axis_rotate_X = 0;
+            this.axis_rotate_Y = 0;
+            this.axis_rotate_Z = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.axis_rotate_X = this.axis_rotate_X + 5f;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.axis_rotate_Y = this.axis_rotate_Y + 5f;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.axis_rotate_Z = this.axis_rotate_Z + 5f;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.axis_rotate_X = this.axis_rotate_X -5f;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.axis_rotate_Y = this.axis_rotate_Y - 5f;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.axis_rotate_Z = this.axis_rotate_Z - 5f;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+            
+            gl.Clear(SharpGL.OpenGL.GL_COLOR_BUFFER_BIT | SharpGL.OpenGL.GL_DEPTH_BUFFER_BIT);  // Очистка скрина
+           
+            gl.LoadIdentity();    // сброс
+
+            gl.Translate(0.0f, 0.0f, -6.0f);  // переместить влево
+
+            gl.Rotate(axis_rotate_X, 1.0f, 0.0f, 0.0f);
+            gl.Rotate(axis_rotate_Y, 0.0f, 1.0f, 0.0f);
+            gl.Rotate(axis_rotate_Z, 0.0f, 0.0f, 1.0f);
+
+            gl.Begin(SharpGL.OpenGL.GL_TRIANGLES); // начать рисовать пирамиду
+
+            gl.Color(1.0f, 0.0f, 0.0f);   //красный 
+            gl.Vertex(0.0f, 1.0f, 0.0f); // центр пирамиды
+            gl.Color(0.0f, 1.0f, 0.0f); //Зеленый
+            gl.Vertex(-1.0f, -1.0f, 1.0f);  // левая сторона пирамиды
+            gl.Color(0.0f, 0.0f, 1.0f);  // Синий
+            gl.Vertex(1.0f, -1.0f, 1.0f);  // права сторона пирамиды
+
+            gl.Color(1.0f, 0.0f, 0.0f);   //красный 
+            gl.Vertex(0.0f, 1.0f, 0.0f); // центр пирамиды
+            gl.Color(0.0f, 0.0f, 1.0f); //Зеленый
+            gl.Vertex(1.0f, -1.0f, 1.0f);  // левая сторона пирамиды
+            gl.Color(0.0f, 1.0f, 0.0f);  // Синий
+            gl.Vertex(1.0f, -1.0f, -1.0f);  // права сторона пирамиды
+
+            gl.Color(1.0f, 0.0f, 0.0f);   //красный 
+            gl.Vertex(0.0f, 1.0f, 0.0f); // центр пирамиды
+            gl.Color(0.0f, 1.0f, 0.0f); //Зеленый
+            gl.Vertex(1.0f, -1.0f, -1.0f);  // левая сторона пирамиды
+            gl.Color(0.0f, 0.0f, 1.0f);  // Синий
+            gl.Vertex(-1.0f, -1.0f, -1.0f);  // права сторона пирамиды
+
+            gl.Color(1.0f, 0.0f, 0.0f);   //красный 
+            gl.Vertex(0.0f, 1.0f, 0.0f); // центр пирамиды
+            gl.Color(0.0f, 0.0f, 1.0f); //Зеленый
+            gl.Vertex(-1.0f, -1.0f, -1.0f);  // левая сторона пирамиды
+            gl.Color(0.0f, 1.0f, 0.0f);  // Синий
+            gl.Vertex(-1.0f, -1.0f, 1.0f);  // права сторона пирамиды
+
+
+
+            gl.End();
+            gl.LoadIdentity();
+        }
+
+        private void openGLControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ openGL-example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("openGL-example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("openGL-example")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7b081f7d-0c5a-4896-adeb-3c78b39dff24")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
